bfa078ba6dd3583adf5470e60b731f327631755d
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-05-07 09:11:09 -0500

    got rid of old unused code

--- print_board.cpp
+++ print_board.cpp
@@ -8,17 +8,7 @@
     this->board = inputted_board;
   }
 
-  string ConsoleBoardCreator::formatted_board()
-  {
-    // return "  " + this->board->get_mark(1) + " | " + this->board->get_mark(2) + " | " + this->board->get_mark(3) + "\n" +
-    
-    // " ----------- " + "\n  " +
-
-    // this->board->get_mark(4) + " | " + this->board->get_mark(5) + " | " + this->board->get_mark(6) + "\n" +
-    
-    // " ----------- " + "\n  " +
-
-    // this->board->get_mark(7) + " | " + this->board->get_mark(8) + " | " + this->board->get_mark(9) + "\n";
+  string ConsoleBoardCreator::formatted_board() {
 
     string printed_board;
     int counter = 1;
