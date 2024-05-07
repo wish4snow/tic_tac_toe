48d00a608dddcb2b0b45c17fc8d39654ed9c6a9a
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-05-07 17:09:15 -0500

    computer player menu now working

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
-// #include <iostream>
-// #include "board.cpp"
-//#include "print_board.h"
+
 #include "game.h"
 #include "player.hpp"
 #include "human_player.hpp"
 #include "computer_player.hpp"
-#include "exit_menu.hpp"
+
+using namespace std;
 
 int main()
 {
-  Board board = Board(10);
+  Board board = Board();
   Board *board_ptr = &board;
 
   ConsoleBoardCreator creator = ConsoleBoardCreator(board_ptr);
   ConsoleBoardCreator *creator_ptr = &creator;
 
-  //cout << creator.formatted_board() << endl;
-
   Rules rules = Rules(board_ptr);
   Rules *rules_ptr = &rules;
 
-  //Player player_one;
-  // Player player_two;
-    HumanPlayer player_one = HumanPlayer(&board, 'X');
-    HumanPlayer player_two = HumanPlayer(&board, 'O');
-
+  HumanPlayer player_one = HumanPlayer(&board, 'X');
   HumanPlayer *player_one_ptr = &player_one;
-  HumanPlayer *player_two_ptr = &player_two;
+  Player *player_two_ptr;
 
+  cout << "Hello, welcome to tic tac toe!" << endl;
+  cout << "Enter a number to chose mode.\n" << endl;
+  cout << "1: human vs human" << endl;
+  cout << "2: human vs computer\n\n:";
 
+  string user_input;
+  cin >> user_input;
 
+  while (!(stoi(user_input) == 1) && !(stoi(user_input) == 2)) {
+    cout << "Please enter a valid input\n\n";
+    cout << "Enter a number to chose mode.\n" << endl;
+    cout << "1: human vs human" << endl;
+    cout << "2: human vs computer\n:";
 
-  ExitMenu::start_menu(board_ptr, player_one_ptr, player_two_ptr);
+    cin >> user_input;
+  }
+
+  if (stoi(user_input) == 1) {
+
+    player_two_ptr = new HumanPlayer(&board, 'O');
+
+  } else if (stoi(user_input) == 2){
+
+    player_two_ptr = new ComputerPlayer(&board, 'O');
+  }
 
   Game game = Game(board_ptr, rules_ptr, creator_ptr, player_one_ptr, player_two_ptr);
   game.start();
